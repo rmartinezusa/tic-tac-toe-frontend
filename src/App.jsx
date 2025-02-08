0a88dd16c62a0bed32c8773472ffaa86bb087f53
Rogelio Martinez <rogejr89@gmail.com>
2025-02-07 22:05:04 -0500

    set up for all files and folders I will need.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
+import { RouterProvider } from 'react-router-dom';
+import router from './router';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-    </>
-  )
+    return <RouterProvider router={router} />;
 }
 
 export default App
---
+++ src/router.jsx
@@ -0,0 +1,19 @@
+import { createBrowserRouter } from "react-router-dom";
+
+import Root from "./layout/Root";
+
+import Home from "./components/Home";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Root />,
+        children:[
+            {
+                path: "/",
+                element:<Home/>,
+            }
+        ]
+    }
+]); 
+export default router;
