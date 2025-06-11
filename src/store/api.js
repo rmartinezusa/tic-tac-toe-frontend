26ccd60c9081de5d30e3e0b5671787fe11b7f373
Rogelio Martinez <rogejr89@gmail.com>
2025-06-11 14:02:41 -0400

    D1

--- src/store/api.js
+++ src/store/api.js
@@ -2,7 +2,8 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const api = createApi({
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:3000",
+    // baseUrl: "http://localhost:3000",
+    baseUrl: "https://tic-tac-toe-backend-a6zz.onrender.com",
     prepareHeaders: (headers, { getState }) => {
       const token = getState().auth.token;
       token && headers.set("authorization", `Bearer ${token}`);
